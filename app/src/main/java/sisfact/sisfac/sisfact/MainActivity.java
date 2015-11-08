0cd4f1f1a3b8cea78e18454925d801d672bb34b2
Cesardarinel <Cesardarinelortiz@gmail.com>
2015-11-07 21:03:31 -0400

    sfinuevo/ prueba del menu

--- app/src/main/java/sisfact/sisfac/sisfact/MainActivity.java
+++ app/src/main/java/sisfact/sisfac/sisfact/MainActivity.java
@@ -6,32 +6,69 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-
-    private Button boton;
-    private EditText usuario;
-    private EditText contrasena;
+    //botones del menu
+    ImageButton  factura,producto,cliente,suplidor,cpp,reporte;
+    Intent nuevaActividad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //inicialisamos variables
+        factura= (ImageButton )findViewById(R.id.factura);
+        producto= (ImageButton )findViewById(R.id.productos);
+        cliente= (ImageButton )findViewById(R.id.cliente);
+        suplidor= (ImageButton )findViewById(R.id.suplidor);
+        cpp= (ImageButton )findViewById(R.id.cpp);
+        reporte= (ImageButton )findViewById(R.id.reporte);
+        //----------------setOnClickListener--------------------
+        factura.setOnClickListener(this);
+        producto.setOnClickListener(this);
+        cliente.setOnClickListener(this);
+        suplidor.setOnClickListener(this);
+        cpp.setOnClickListener(this);
+        reporte.setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View v) {
-        if (true) {
 
-                Intent menuprin = new Intent(this, MenuPrincipal.class);
-                startActivity(menuprin);
-                this.finish();
+        switch (v.getId()) {
+            case R.id.factura:
+                nuevaActividad = new Intent(this, MenuPrincipal.class);
+                startActivity(nuevaActividad);
 
+                break;
+            case R.id.productos:
+                nuevaActividad = new Intent(this, MenuPrincipal.class);
+                startActivity(nuevaActividad);
 
-            Intent intent =  new Intent(this,sisfact.sisfac.sisfact.guardar.Contacto.class);
-            startActivity(intent);
+                break;
+            case R.id.cliente:
+                nuevaActividad = new Intent(this, MenuPrincipal.class);
+                startActivity(nuevaActividad);
 
+                break;
+            case R.id.suplidor:
+                nuevaActividad = new Intent(this, MenuPrincipal.class);
+                startActivity(nuevaActividad);
+
+                break;
+            case R.id.cpp:
+                nuevaActividad = new Intent(this, MenuPrincipal.class);
+                startActivity(nuevaActividad);
+
+                break;
+            case R.id.reporte:
+                nuevaActividad = new Intent(this, MenuPrincipal.class);
+                startActivity(nuevaActividad);
+
+            default:
+                break;
         }
     }
 }
