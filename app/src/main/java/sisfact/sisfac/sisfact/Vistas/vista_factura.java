372e6643d5c2313bedae44d14ffb2d5b414f61e4
Miguel German <milo2204@gmail.com>
2015-11-26 16:26:00 -0400

    Cambios de factura

--- app/src/main/java/sisfact/sisfac/sisfact/Vistas/vista_factura.java
+++ app/src/main/java/sisfact/sisfac/sisfact/Vistas/vista_factura.java
@@ -4,11 +4,24 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import sisfact.sisfac.sisfact.R;
 
-public class vista_factura extends AppCompatActivity implements View.OnClickListener {
+public class vista_factura extends AppCompatActivity implements View.OnClickListener,AdapterView.OnItemSelectedListener {
+
+    public Button agregar;
+    public Spinner spin;
+    public List<String> listaTipo = new ArrayList<String>();
+
     /**
      *
      * @param savedInstanceState
@@ -17,6 +30,24 @@ public class vista_factura extends AppCompatActivity implements View.OnClickList
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vista_factura);
+
+        agregar = (Button) findViewById(R.id.button_agregar_lineas);
+        spin = (Spinner) findViewById(R.id.spinner_tipo_producto);
+
+        agregar.setOnClickListener(this);
+        spin.setOnItemSelectedListener(this);
+
+        //agregando valores a la lista de Productos
+        listaTipo.add("--Ninguno--");
+        listaTipo.add("Camisa");
+        listaTipo.add("Pantalon");
+        listaTipo.add("Ropa Interior");
+        listaTipo.add("Zapatos");
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,listaTipo);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin.setAdapter(dataAdapter);
+
     }
 
     /**
@@ -25,11 +56,23 @@ public class vista_factura extends AppCompatActivity implements View.OnClickList
      */
     @Override
     public void onClick(View v){
-        if (v.getId()==R.id.button_agregar_lineas) {
-
-              Intent  nuevaActividad = new Intent(this, vista_factura_lineas.class);
-                startActivity(nuevaActividad);
+        if (v.getId()==R.id.button_agregar_lineas){
+            Intent  nuevaActividad = new Intent(this, vista_factura_lineas.class);
+            nuevaActividad.putExtra("spinItem",spin.getSelectedItem().toString());
+            startActivity(nuevaActividad);
         }
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String item = parent.getItemAtPosition(position).toString();
+        // Showing selected spinner item
+        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 }
